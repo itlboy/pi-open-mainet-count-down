087d9c970a55f90165338213da55c13831b6a766
itlboy <nkien.bk@gmail.com>
2025-02-13 09:56:11 +0700

    update build

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require("gulp");
+const rev = require("gulp-rev").default;  // Fix lỗi
+const revReplace = require("gulp-rev-replace");
+
+gulp.task("revision", function () {
+    return gulp.src(["styles.css", "script.js"])
+        .pipe(rev()) // Fix lỗi
+        .pipe(gulp.dest("dist"))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("replace", function () {
+    const manifest = gulp.src("dist/rev-manifest.json");
+    return gulp.src("index.html")
+        .pipe(revReplace({ manifest: manifest }))
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", gulp.series("revision", "replace"));
