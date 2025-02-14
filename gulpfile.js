b6d4eecd6fddfa113680cc8293fbe9bf81c3368b
itlboy <nkien.bk@gmail.com>
2025-02-14 14:34:18 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,29 @@ const gulp = require("gulp");
 const rev = require("gulp-rev");
 const revReplace = require("gulp-rev-replace");
 
-// Task copy file ảnh & các file tĩnh vào dist/
-gulp.task("copyStaticFiles", function () {
-    return gulp.src(["pi.webp"]) // Copy ảnh pi.webp 
+// Task copy ảnh & các file tĩnh vào `dist/`
+function copyStaticFiles() {
+    return gulp.src("pi.webp") // Copy ảnh pi.webp
         .pipe(gulp.dest("dist"));
-});
+}
 
 // Task tạo phiên bản file CSS & JS
-gulp.task("revision", function () {
-    return gulp.src(["styles.css", "script.js"])
-        .pipe(rev())
+function revision() {
+    return gulp.src(["styles.css", "script.js"], { base: "." }) // Giữ cấu trúc thư mục
+        .pipe(rev()) // Thêm hash version
         .pipe(gulp.dest("dist"))
         .pipe(rev.manifest())
         .pipe(gulp.dest("dist"));
-});
+}
 
 // Task thay thế đường dẫn phiên bản mới vào HTML
-gulp.task("replace", function () {
+function replace() {
     const manifest = gulp.src("dist/rev-manifest.json");
     return gulp.src("index.html")
         .pipe(revReplace({ manifest: manifest }))
         .pipe(gulp.dest("dist"));
-});
+}
 
-// Task mặc định: Chạy cả copyStaticFiles + revision + replace
-gulp.task("default", gulp.series("copyStaticFiles", "revision", "replace"));
+// Task mặc định chạy cả 3 bước
+const build = gulp.series(copyStaticFiles, revision, replace);
+exports.default = build;
